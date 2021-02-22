227e8553499d90a01f28d0b30e155d4d9cf47911
Febri <achmadfebryanto@gmail.com>
2021-02-22 07:21:15 +0700

    Wrap the functions in a class

--- moodle-scraper.py
+++ moodle-scraper.py
@@ -1,36 +1,16 @@
-import requests
-import os
-import urllib3
+from urllib3 import disable_warnings
+from urllib3.exceptions import NewConnectionError, MaxRetryError, InsecureRequestWarning
+from requests.exceptions import ConnectionError
+from socket import gaierror
 from bs4 import BeautifulSoup
 from moodle_kit import Moodle
 from getpass import getpass
 from collections import defaultdict
 from tabulate import tabulate
+import requests
+import os
 import concurrent.futures
-from socket import gaierror
-from urllib3.exceptions import NewConnectionError, MaxRetryError
-from requests.exceptions import ConnectionError
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-
-def get_request(url, headers):
-    '''Perform GET HTTP request to the login page.'''
-
-    response = requests.get(
-        url,
-        headers=headers,
-        allow_redirects=True
-    )
-
-    return response
-
-
-def get_course_urls(parsed_response):
-    '''Retrieves each course's url and store them in a list.'''
-
-    a = (parsed_response.find_all('a', class_="list-group-item", href=True))
-    urls = [x.get('href') for x in a if 'course' in x.get('href')]
-    return urls
+disable_warnings(InsecureRequestWarning)
 
 
 class Course:
@@ -44,145 +24,160 @@ class Course:
         return f'<{self.course_name}>'
 
 
-def is_assignment(item_type):
-    '''Check if an item is an assignement.'''
+class MyMoodle(Moodle):
 
-    if item_type.lower() == 'assignment':
-        return True
-    return False
+    def __init__(self) -> None:
+        super().__init__()
 
+    def get_request(self, url):
+        '''Perform GET HTTP request to the login page.'''
 
-def get_deadline(item_url, headers):
-    '''Return the item's deadline only if it's an assignment'''
+        response = requests.get(
+            url,
+            headers=self.request_headers,
+            allow_redirects=True
+        )
+        return response
 
-    get_response = BeautifulSoup(get_request(
-        item_url, headers).text, 'html.parser')
-    return get_response
+    def get_course_urls(self, soup):
+        '''Retrieves each course's url and store them in a list.'''
 
+        a_tags = (soup.find_all('a', class_="list-group-item", href=True))
+        urls = [x.get('href') for x in a_tags if 'course' in x.get('href')]
+        return urls
 
-def multithread(func, items):
-    '''Run repeated processes with multithreading capability.'''
+    def is_assignment(self, item_type):
+        '''Check if an item is an assignement.'''
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
-        return executor.map(func, items)
+        if item_type.lower() == 'assignment':
+            return True
+        return False
 
+    def get_deadline(self, item_url):
+        '''Return the item's deadline only if it's an assignment'''
 
-def fetch_courses(course_urls, headers):
-    '''Extract information about each course (name, sections, section items)'''
+        soup = BeautifulSoup(self.get_request(
+            item_url).text, 'html.parser')
+        return soup
 
-    def get_course_info(url):
-        response = get_request(url, headers)
-        if response.ok:
-            parsed_response = BeautifulSoup(response.text, 'html.parser')
+    def multithread(self, func, items):
+        '''Run repeated processes with multithreading capability.'''
 
-            # Get course name
-            course_name = [x.strip()
-                           for x in parsed_response.title.text.split('-')]
-            course_name = f'[{course_name[0].split()[1]}]{course_name[1]} - {course_name[-1]}'
+        with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+            return executor.map(func, items)
 
-            # Get course's sections and store them in a list
-            course_sections = parsed_response.select(
-                '.content > .sectionname > span > a')
-            course_sections = [x.string for x in course_sections]
+    def fetch_courses(self, course_urls):
+        '''Extract information about each course (name, sections, section items)'''
 
-            # Get the items of each section
-            section_items = defaultdict(list)
-            for i, section_name in enumerate(course_sections):
-                items = parsed_response.select(
-                    f'#section-{i}>div:nth-child(3)>ul:nth-child(4)>li>div>div>div:nth-child(2)>div>a')
+        def get_course_info(url):
+            response = self.get_request(url)
+            if response.ok:
+                soup = BeautifulSoup(response.text, 'html.parser')
 
-                item_texts = [x.text.split() for x in items]
-                item_names = [' '.join(x[:-1]) for x in item_texts]
-                item_types = [x[-1] for x in item_texts]
-                item_urls = [x.get('href') for x in items]
+                # Get course name
+                course_name = [x.strip()
+                               for x in soup.title.text.split('-')]
+                course_name = f'[{course_name[0].split()[1]}]{course_name[1]} - {course_name[-1]}'
 
-                for item_name, item_url, item_type in zip(item_names, item_urls, item_types):
-                    deadline = ''
-                    if is_assignment(item_type):
-                        get_response = get_deadline(item_url, headers)
-                        deadline = get_response.select_one(
-                            '.submissionstatustable>div:nth-child(2)>table>tbody>tr:nth-child(4)>td').text
-                        if 'submitted' in deadline:
-                            deadline = 'SUBMITTED'
-                    else:
-                        deadline = 'NOT an assignment'
-                    section_items[section_name].append(
-                        [item_name, item_url, deadline])
+                # Get course's sections and store them in a list
+                course_sections = soup.select(
+                    '.content > .sectionname > span > a')
+                course_sections = [x.string for x in course_sections]
 
-            return Course(course_name, section_items)
+                # Get the items of each section
+                section_items = defaultdict(list)
+                for i, section_name in enumerate(course_sections):
+                    items = soup.select(
+                        f'#section-{i}>div:nth-child(3)>ul:nth-child(4)>li>div>div>div:nth-child(2)>div>a')
 
-    course_objects = multithread(get_course_info, course_urls)
-    return course_objects
+                    item_texts = [x.text.split() for x in items]
+                    item_names = [' '.join(x[:-1]) for x in item_texts]
+                    item_types = [x[-1] for x in item_texts]
+                    item_urls = [x.get('href') for x in items]
 
+                    for item_name, item_url, item_type in zip(item_names, item_urls, item_types):
+                        deadline = ''
+                        if self.is_assignment(item_type):
+                            get_response = self.get_deadline(item_url)
+                            deadline = get_response.select_one(
+                                '.submissionstatustable>div:nth-child(2)>table>tbody>tr:nth-child(4)>td').text
+                            if 'submitted' in deadline:
+                                deadline = 'SUBMITTED'
+                        else:
+                            deadline = 'NOT an assignment'
+                        section_items[section_name].append(
+                            [item_name, item_url, deadline])
 
-def format_section_name(name):
-    '''Format each section's name that doesn't align to the common pattern.'''
+                return Course(course_name, section_items)
 
-    try:
-        template = 'Pertemuan'
-        if name != 'General' and template not in name:
-            name = name.split('.')[0]
-            return f'{template} {int(name)}'
-        return name
-    except:
-        return name
+        course_objects = self.multithread(get_course_info, course_urls)
+        return course_objects
 
+    def format_section_name(self, name):
+        '''Format each section's name that doesn't align to the common pattern.'''
 
-def format_item_name(name):
-    '''Split the item's long ugly name and return the longest one.'''
+        try:
+            template = 'Pertemuan'
+            if name != 'General' and template not in name:
+                name = name.split('.')[0]
+                return f'{template} {int(name)}'
+            return name
+        except:
+            return name
 
-    try:
-        name = list(map(lambda x: x.strip(), name.split('-')))
-        return max(name, key=len)
-    except:
-        return name
+    def format_item_name(self, name):
+        '''Split the item's long ugly name and return the longest word.'''
 
+        try:
+            name = list(map(lambda x: x.strip(), name.split('-')))
+            return max(name, key=len)
+        except:
+            return name
 
-def generate_summary(course_objects):
-    '''Put everything together in a nicely formatted table.'''
+    def generate_summary(self, course_objects):
+        '''Put everything together in a nicely formatted table.'''
 
-    headers = ['ID', 'Course Name', 'Recent Section',
-               'Section Items', 'Deadline']
-    table_format = 'fancy_grid'
+        headers = ['ID', 'Course Name', 'Recent Section',
+                   'Section Items', 'Deadline']
+        table_format = 'fancy_grid'
 
-    def unpack_course(course):
-        # Course ID
-        id = course.course_name[:7][1:-1]
+        def unpack_course(course):
+            # Course ID
+            id = course.course_name[:7][1:-1]
 
-        # Course name
-        name = course.course_name[7:]
+            # Course name
+            name = course.course_name[7:]
 
-        # Section name
-        recent_section = format_section_name(
-            list(course.section_items.keys())[-1])
+            # Section name
+            recent_section = self.format_section_name(
+                list(course.section_items.keys())[-1])
 
-        section_items = ''
-        deadline = ''
+            section_items = ''
+            deadline = ''
 
-        items = list(course.section_items.values())[-1]
-        for item_name, item_url, item_deadline in items:
-            section_items += format_item_name(item_name) + \
-                '\n' + item_url + '\n\n'
-            deadline += item_deadline + '\n\n\n'
+            items = list(course.section_items.values())[-1]
+            for item_name, item_url, item_deadline in items:
+                section_items += self.format_item_name(item_name) + \
+                    '\n' + item_url + '\n\n'
+                deadline += item_deadline + '\n\n\n'
 
-        return [id, name, recent_section, section_items, deadline]
+            return [id, name, recent_section, section_items, deadline]
 
-    tables = multithread(unpack_course, course_objects)
-    return tabulate(tables, headers=headers, tablefmt=table_format, colalign=('left', 'left', 'left', 'left'))
+        tables = self.multithread(unpack_course, course_objects)
+        return tabulate(tables, headers=headers, tablefmt=table_format, colalign=('left', 'left', 'left', 'left'))
 
+    def print_error_message(self, messages=[]):
+        '''Print coustomized error messages.'''
 
-def print_error_message(messages=[]):
-    '''Print coustomized error messages.'''
-
-    if isinstance(messages, str):
-        messages = [messages]
-    if len(messages) > 1:
-        print()
-        for message in messages:
-            print(f'[!] {message}')
-    else:
-        print(f'\n\n[!] {messages[0]}')
-    exit('\n[!] Program exited.\n')
+        if isinstance(messages, str):
+            messages = [messages]
+        if len(messages) > 1:
+            print()
+            for message in messages:
+                print(f'[!] {message}')
+        else:
+            print(f'\n\n[!] {messages[0]}')
+        exit('\n[!] Program exited.\n')
 
 
 def main():
@@ -202,38 +197,36 @@ def main():
         while not password:
             password = getpass('[!] Password\t\t= ')
 
-        moodle = Moodle()
-        response = moodle.login(login_url, username, password)
-        headers = response.request.headers
+        moodle = MyMoodle()
+        moodle.login(login_url, username, password)
 
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(moodle.response.text, 'html.parser')
         today = soup.find(
             'td', class_='today').a.get('aria-label')
         print(today)
 
         # Course urls extraction after the successful login
-        course_urls = get_course_urls(soup)
-        course_objects = fetch_courses(course_urls, headers)
+        course_urls = moodle.get_course_urls(soup)
+        course_objects = moodle.fetch_courses(course_urls)
 
         # Print summary table
         print()
-        print(generate_summary(course_objects))
+        print(moodle.generate_summary(course_objects))
         print()
 
-        # Logout
         moodle.logout()
         exit()
 
     except (gaierror, NewConnectionError, MaxRetryError, ConnectionError) as e:
-        print_error_message([
+        moodle.print_error_message([
             '[!] A connection error occured! It might be your slow bandwidth.',
             '[!] Fix your connection and try again!'])
 
     except KeyboardInterrupt as e:
-        print_error_message(['[-] Program was interrupted!'])
+        moodle.print_error_message(['[-] Program was interrupted!'])
 
     except Exception as e:
-        print_error_message([e.__str__()])
+        moodle.print_error_message([e.__str__()])
 
 
 if __name__ == "__main__":
